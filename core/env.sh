c6c6ac24dced750e639843f74ac46411faf8a338
雨初 <zliu9732@gmail.com>
2026-07-21 17:55:47 +0800

    feat: add 3 more GitHub mirrors (7 total), v4.4.4

--- core/env.sh
+++ core/env.sh
@@ -75,5 +75,5 @@ load_config() {
     fi
 
     # GitHub 镜像默认值（配置未提供时使用）
-    : "${GITHUB_MIRRORS:=https://ghproxy.net/ https://gh.api.99988866.xyz/ https://github.moeyy.xyz/ https://gh.llkk.cc/}"
+    : "${GITHUB_MIRRORS:=https://ghproxy.net/ https://gh.api.99988866.xyz/ https://github.moeyy.xyz/ https://gh.llkk.cc/ https://mirror.ghproxy.com/ https://gh.ddlc.com/ https://gh-proxy.lanqier.me/}"
 }
